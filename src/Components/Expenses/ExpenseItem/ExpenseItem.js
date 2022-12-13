bb5ae2e532a7e2b0203222fe428f39b5ecc87148
Yasin <mdyasinansari901@gmail.com>
2022-12-13 13:16:55 +0530

    added the Expenses component

---
+++ src/Components/Expenses/ExpenseItem/ExpenseItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ExpenseDate from './ExpenseDate/ExpenseDate.js';
+import './ExpenseItem.css';
+import ExpensePrice from './ExpensePrice/ExpensePrice.js';
+
+
+const ExpenseItem =(props)=>{
+    return (
+
+        <div className='expense-item'>
+            {/* <h1>ExpenseDate</h1> */}
+            <ExpenseDate date={props.date}></ExpenseDate>
+            <div className='expense-item-Description'>
+                <h2>{props.title}</h2>
+                {/* <div className='expense-item-price'>
+                    <h2>{props.price}</h2>
+                </div> */}
+                <ExpensePrice price={props.price}></ExpensePrice>
+            </div>
+
+        </div>
+    )
+}
+export default ExpenseItem;

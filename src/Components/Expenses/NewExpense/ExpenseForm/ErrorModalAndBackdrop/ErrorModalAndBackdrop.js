5730a1f5d95f7b6bb420cc7ca66615081caf882c
Yasin <mdyasinansari901@gmail.com>
2022-12-31 11:56:12 +0530

    added react potals and react fragments in errormodal.js file

--- src/Components/Expenses/NewExpense/ExpenseForm/ErrorModalAndBackdrop/ErrorModalAndBackdrop.js
+++ src/Components/Expenses/NewExpense/ExpenseForm/ErrorModalAndBackdrop/ErrorModalAndBackdrop.js
@@ -1,12 +1,57 @@
 import React from 'react';
+import  ReactDOM  from 'react-dom';
 import classes from './ErrorModalAndBackdrop.module.css';
 
-const ErrorModalAndBackdrop =(props)=>{
-    return (
 
-        <div>
-            <div className={classes.backdrop}  onClick={props.click}></div>
-            <div className={classes.modal}>
+
+
+// const ErrorModalAndBackdrop =(props)=>{
+//     return (
+
+//         <div>
+//             <div className={classes.backdrop}  onClick={props.click}></div>
+//             <div className={classes.modal}>
+//                <header className={classes.header} >
+//                 <h2>
+//                  {props.title}
+//                 </h2>
+//                </header>
+//                <div className={classes.content}>
+//                 <p>
+//                    {props.message}
+//                 </p>
+//                 </div>
+//                 <footer className={classes.actions}>
+//                 <button onClick={props.click}>
+//                        okay
+//                 </button>
+//                </footer>
+//          </div>
+//         </div>
+
+//     )
+// }
+
+
+
+
+
+
+
+                                            // OR
+                                    // (USING REACT PORTALS)
+
+        const Backdrop=(props)=>{
+            return(
+
+                <div className={classes.backdrop}  onClick={props.press}></div>
+
+            )
+        }
+
+        const Modal=(props)=>{
+            return (
+                <div className={classes.modal}>
                <header className={classes.header} >
                 <h2>
                  {props.title}
@@ -23,8 +68,26 @@ const ErrorModalAndBackdrop =(props)=>{
                 </button>
                </footer>
          </div>
-        </div>
+            )
+        }
+
+
+
+
+        
+const ErrorModalAndBackdrop =(props)=>{
+    return (
+
+
+        <React.Fragment>
+            {ReactDOM.createPortal(<Backdrop press={props.click}/>, document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(<Modal title={props.title} message={props.message}   click={props.click}/>, document.getElementById('modal-root'))}
+        </React.Fragment>
 
     )
 }
+
+
+
+
 export default ErrorModalAndBackdrop;
